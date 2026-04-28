05454c8582f64705c713a32f3945ee47ff306c55
Hritik sharma <hs1349507@gmail.com>
2026-04-28 10:24:33 +0530

    fix: updated next.config COOP headers source matcher

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -5,12 +5,16 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: '/:path*',
         headers: [
           {
             key: 'Cross-Origin-Opener-Policy',
             value: 'same-origin-allow-popups',
           },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'unsafe-none',
+          }
         ],
       },
     ];
